ac48bd53fab70c4bcd80be3fbcbbfadf2c719c3f
Saurav Kumar <saurav.kumar2@kantar.com>
2023-07-16 01:08:15 +0530

    spring boot application is secure with spring security and jwt token

---
+++ employees-application/src/main/java/com/tipsontech/employeesapp/config/JwtSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.tipsontech.employeesapp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.tipsontech.employeesapp.security.JwtAuthenticationEntryPoint;
+import com.tipsontech.employeesapp.security.JwtAuthenticationFilter;
+
+@Configuration
+public class JwtSecurityConfig {
+
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+		return httpSecurity.csrf(CsrfConfigurer::disable)
+				.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+						.requestMatchers("/api/v1/user", "/api/v1/auth/login").permitAll())
+				.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+						.requestMatchers("/api/v1/employees", "/api/v1/employees/**").authenticated())
+				.exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class).build();
+
+	}
+}
